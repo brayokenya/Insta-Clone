5bc27e3f69be56321379537ed7d2368d2bfa87ab
brayokenya <kiirubrian21@gmail.com>
2020-07-03 22:49:30 +0300

    added templates and views

--- clone/settings.py
+++ clone/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'insta',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,12 @@ WSGI_APPLICATION = 'clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'briankiiru',
+        'PASSWORD': 'andela',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to Instagram')
+    return render(request, 'index.html')
+
+def posts_of_day(request):
+    date = dt.date.today()
+    return render(request, 'insta-post/posts_today.html', {"date": date,})
+
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
