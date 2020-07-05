2f8503ede11436056532fc19626e4095b492d167
brayokenya <kiirubrian21@gmail.com>
2020-07-05 09:59:30 +0300

    modified views

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
+from .models import Update, Location, Category
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -8,10 +11,20 @@ import datetime as dt
 # Create your views here.
 def welcome(request):
     return render(request, 'index.html')
-
+    
+@login_required(login_url='/accounts/login/')
 def posts_of_day(request):
     date = dt.date.today()
-    return render(request, 'insta-post/posts_today.html', {"date": date,})
+    insta = Update.todays_insta()
+    locations = Location.objects.all()
+    return render(request, 'insta-post/posts_today.html', {"date": date, "insta": insta, "locations": locations})
+
+
+def location(request,location):
+    locations = Location.objects.all()
+    selected_location = Location.objects.get(id = location)
+    insta = Update.objects.filter(location = selected_location.id)
+    return render(request, 'insta-post/location.html', {"location":selected_location,"locations":locations,"insta":insta})    
 
 
 def convert_dates(dates):
@@ -24,3 +37,4 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
